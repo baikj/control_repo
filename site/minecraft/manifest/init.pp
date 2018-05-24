3bfe17d69a70be4d71b75741cc8424803b0f056d
Jay BAIK <ethiansjh@icloud.com>
2018-05-24 11:33:12 +0200

    Update init.pp

--- site/minecraft/manifest/init.pp
+++ site/minecraft/manifest/init.pp
@@ -1,1 +1,19 @@
-
+class minecraft {
+  file {'/opt/minecraft':
+    ensure  =>  directory,
+  }
+  
+  file {'/opt/minecraft/eula.txt':
+    ensure  =>  file,
+    content =>  'eula=true',
+  }
+  
+  package {'default-jre':
+    ensure  =>  present,
+  }
+  
+  service {'minecraft':
+    ensure  =>  running,
+    enable  =>  true,
+  }
+}
