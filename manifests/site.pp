5a979c4b5c5825abc30b1246a022702ac812dae2
Jay BAIK <ethiansjh@icloud.com>
2018-05-24 11:57:11 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,21 @@ node default {
 
 node 'puppet' {
   include role::master_server
+  
+  file { '/root/README':
+    ensure  =>  present,
+    content =>  "Ceci est un fichier lisez-moi.\n",
+  }
+}
+
+node /^client/ {
+  include role::client_role
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node 'mine2' {
+  include role::minecraft_server
 }
